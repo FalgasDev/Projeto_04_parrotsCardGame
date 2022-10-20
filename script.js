e856871e55cd0a4be46cb297da73824b6e7e3337
FabioGarroteDev <fgarrote12@gmail.com>
2022-10-20 16:04:32 -0300

    Adicionado a distribuição das cartas embaralhadas

--- script.js
+++ script.js
@@ -1,26 +1,33 @@
 const cards = ['bobrossparrot', 'explodyparrot', 'fiestaparrot', 'metalparrot', 'revertitparrot', 'tripletsparrot', 'unicornparrot'];
+const cheap = [];
+
+cards.sort(shuffle)
 
 let numberOfCards = Number(prompt('Quantas cartas você quer?'));
 const card = document.querySelector('.cards');
-
-cards.sort(shuffle);
-
 
 while (numberOfCards < 4 || numberOfCards > 14 || numberOfCards % 2 != 0) {
     alert('insira um número par de 4 a 14 por favor!')
     numberOfCards = Number(prompt('Quantas cartas você quer?'));
 }
 
-for (let i = 0; numberOfCards > i; i++) {  
+for (let i = 0; i < (numberOfCards / 2); i++) {
+    cheap.push(cards[i])
+    cheap.push(cards[i])
+}
+
+cheap.sort(shuffle);
+for (let i = 0; numberOfCards > i; i++) {
     card.innerHTML += `
     <div class="card" onclick="flipCard(this)">
         <div class="front hidden">
-            <img src="./images/${cards[i]}.gif">
+            <img src="./images/${cheap[i]}.gif">
         </div>
         <div class="back">
             <img src="./images/back.png">
         </div>
-    </div>`
+    </div>
+    `
 }
 
 function flipCard(element) {
